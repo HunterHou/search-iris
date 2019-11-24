4d77c3636a005852ab07e5e8e12bf5be8a7ff0af
HunterHou <183598821@qq.com>
2019-11-25 00:02:00 +0800

    init

--- src/main/main.go
+++ src/main/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	var baseDir = "F:\\emby"
+	var baseDir = "F:\\emby\\tomake\\秋山祥子\\inmad\\[秋山祥子] [ATID-349]女教師玩具化計画 秋山祥子"
 	items := service.Walk(baseDir)
 	for i := 0; i < len(items); i++ {
 		fmt.Println(items[i])
