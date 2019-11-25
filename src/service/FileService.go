0d6a4a696030dfbb37025f8d6b9385dc1597bf60
hunter <183598821@qq.com>
2019-11-25 10:23:08 +0800

    hd

--- src/service/FileService.go
+++ src/service/FileService.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"filepath"
 	"io/ioutil"
 
 	"../cons"
@@ -10,37 +11,23 @@ import (
 
 var videoTypes = []string{cons.AVI, cons.MKV, cons.WMV, cons.MP4}
 
+//遍历目录 获取文件库
 func Walk(baseDir string) []model.File {
 	var result []model.File
 	files, _ := ioutil.ReadDir(baseDir)
-	for _, filePath := range files {
-		if filePath.IsDir() {
-			continue
+	for _, path := range files {
+		if path.IsDir() {
+			subDir := filepath.Join(baseDir, path)
+			childResult := Walk(subDir)
 		} else {
-			name := filePath.Name()
+			name := path.Name()
 			suffix := utils.GetSuffux(name)
-			if HasItem(videoTypes, suffix) {
-				file := model.NewFile(name, suffix, filePath.Size(), filePath.ModTime())
+			if utils.HasItem(videoTypes, suffix) {
+				file := model.NewFile(name, suffix, path.Size(), path.ModTime())
 				result = append(result, file)
 			}
 
 		}
 	}
 	return result
-}
-
-func HasItem(lib []string, item string) bool {
-	if lib == nil {
-		return false
-	}
-	if len(lib) == 0 {
-		return false
-	}
-	for i := 0; i < len(lib); i++ {
-		if item == lib[i] {
-			return true
-		}
-	}
-	return false
-
 }
