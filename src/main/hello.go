ab555ecad9583129211c5d10575f26d9f9fc6df4
hunter <183598821@qq.com>
2019-11-27 18:43:02 +0800

    hd

---
+++ src/main/hello.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Fprint(w, "be")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler) //设置访问的路由
+	err := http.ListenAndServe(":8080", nil)
+	if err == nil {
+		log.Fatal("ListenAndServe", err)
+	}
+}
