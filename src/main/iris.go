0b468fed12d0f182780f734d948a8ce4af7bb9fc
hunter <183598821@qq.com>
2019-11-28 12:39:16 +0800

    hd

--- src/main/iris.go
+++ src/main/iris.go
@@ -5,11 +5,25 @@ import "github.com/kataras/iris"
 func main() {
 	app := iris.New()
 	app.Handle("GET", "/", func(ctx iris.Context) {
+		app.Logger().Info(ctx.Path())
 		ctx.HTML("<h1>hello world!!!</h1>")
+
 	})
-	app.Get("/go", func(ctx iris.Context) { ctx.WriteString("hello go") })
-	app.Get("/json", func(ctx iris.Context) { ctx.JSON(iris.Map{"message": "hello json"}) })
-	app.Run(iris.Addr("127.0.0.1:8080"), iris.WithConfiguration(iris.Configuration{
+	app.Get("/hello", func(ctx iris.Context) {
+		app.Logger().Info(ctx.Path())
+		name := ctx.URLParam("name")
+		ctx.WriteString("hello " + name)
+	})
+	app.Get("/welcome", func(ctx iris.Context) {
+		app.Logger().Info(ctx.Path())
+		name := ctx.URLParam("name")
+		ctx.WriteString("hello " + name)
+	})
+	app.Get("/json", func(ctx iris.Context) {
+		app.Logger().Info(ctx.Path())
+		ctx.JSON(iris.Map{"message": "hello json"})
+	})
+	app.Run(iris.Addr("127.0.0.1:8000"), iris.WithConfiguration(iris.Configuration{
 		DisableStartupLog:    false,
 		FireMethodNotAllowed: false,
 		TimeFormat:           "2019-11-10 18:10:33",
