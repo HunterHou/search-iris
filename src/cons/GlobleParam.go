2509cdfa65b704873332363a01d359218e5122a2
HunterHou <183598821@qq.com>
2020-01-19 01:18:55 +0800

    hd

--- src/cons/GlobleParam.go
+++ src/cons/GlobleParam.go
@@ -8,9 +8,9 @@ var isStatic = true
 //初始化 扫描路径
 // var BaseDir = []string{"e:", "e:\\emby\\tomake", "h:\\emby\\tomake"}
 var BaseUrl = "https://www.cdnbus.in/"
-var BaseDir = []string{"d:\\emby"}
 
-//var BaseDir = []string{"E:\\emby3", "e:\\emby\\tomake", "F:\\emby\\emby-rename", "F:\\emby\\tomake", "H:\\emby\\emby-rename", "H:\\emby\\tomake","i:\\emby\\tomake"}
+//var BaseDir = []string{"d:\\emby"}
+var BaseDir = []string{"E:\\emby3", "e:\\emby\\tomake", "F:\\emby\\emby-rename", "F:\\emby\\tomake", "H:\\emby\\emby-rename", "H:\\emby\\tomake", "i:\\emby\\tomake"}
 var Images = []string{PNG, GIF, PNG}
 var Docs = []string{TXT, XML, XLSX}
 var VideoTypes = []string{AVI, MKV, WMV, MP4}
